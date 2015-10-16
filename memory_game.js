3e59d19ff9314bf491dc029ffe807ec238676f8f
adiaguidry <adiaguidry@gmail.com>
2015-10-16 13:36:37 -0700

    updated JS functionality

--- memory_game.js
+++ memory_game.js
@@ -1,6 +1,6 @@
 var first_card_clicked = null;
 var second_card_clicked = null;
-var total_possible_matches = 2;
+var total_possible_matches = 9;
 var match_counter = 0;
 
 
@@ -8,38 +8,50 @@ var match_counter = 0;
 
 function cardClick (element) {
     //hides back of card
-    $(element).addClass('hide');
+    var back = $(element).find('.back');
+    $(back).hide()
+    var front_img = $(element).find('.front').find('img').attr('src');
+    console.log(front_img);
 
     //checks if its first card if true stores src in first_card_clicked
     if(first_card_clicked == null) {
         //stores first_card_clicked var with src
-         first_card_clicked = $(element).prev().find("img");
-       }
+        first_card_clicked = $(element);
+    }
     else{
         // stores src in second_card_clicked var
-        second_card_clicked = $(element).prev().find("img");
+        second_card_clicked = $(element);
+
         //check if we have a match
-             if(first_card_clicked.attr('src') == second_card_clicked.attr('src')){
-                 $(first_card_clicked).addClass('match');
-                 $(second_card_clicked).addClass('match');
-                 console.log('first set ', first_card_clicked, second_card_clicked);
+        if(first_card_clicked.find('.front').find('img').attr('src') == second_card_clicked.find('.front').find('img').attr('src')){
+            $(first_card_clicked).find('.front').addClass('match');
+            $(second_card_clicked).find('.front').addClass('match');
+            console.log('first set ', first_card_clicked.find('.front').find('img').attr('src'), second_card_clicked.find('.front').find('img').attr('src'));
 
-                 first_card_clicked = null;
-                 second_card_clicked = null;
-                 match_counter ++;
-                     if(match_counter == 9){
-                         $('#win').css('visibility', 'visible');
-                     }
+            first_card_clicked = null;
+            second_card_clicked = null;
+            match_counter ++;
+            if(match_counter == 9){
+                $('#win').css('visibility', 'visible');
+            }
 
-                 console.log(match_counter);
-                }
-            else{
-                 first_card_clicked = null;
-                 second_card_clicked = null;
-                console.log('second set ', first_card_clicked, second_card_clicked);
-             }
+            //console.log(match_counter);
         }
+        else {
+            var reset_card_1 = $(first_card_clicked).find('.back');
+            var reset_card_2 = $(second_card_clicked).find('.back');
+            $(reset_card_1).show(1000);
+            $(reset_card_2).show(1000);
+
+            first_card_clicked = null;
+            second_card_clicked = null;
+            console.log('second set ', first_card_clicked, second_card_clicked);
+
+        }
+
+    }
 }
+
 
 
 
