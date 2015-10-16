63c13c88542299b4f76fdf27df2296e1c9661e79
adiaguidry <adiaguidry@gmail.com>
2015-10-15 17:55:45 -0700

    Almost finished with my JS -Adia

--- memory_game.js
+++ memory_game.js
@@ -3,29 +3,45 @@ var second_card_clicked = null;
 var total_possible_matches = 2;
 var match_counter = 0;
 
-$('.card').click(function(){
-
-      $(this).find('.back').css('visibility', 'hidden');
-
-       var first_card_clicked = $(this).find('.front');
-
-        if(first_card_clicked != null) {
-
-            $('.card').click(function(){
-                $(this).find('.back').css('visibility', 'hidden');
-                var second_card_clicked = $(this).find('.front');
-
-            });
-
-         if(first_card_clicked != null && second_card_clicked != null)  {
-             $('.card').click(function(){
-                 alert('nope');
-             })
-         }
 
 
 
-    }
+function cardClick (element) {
+    //hides back of card
+    $(element).addClass('hide');
+
+    //checks if its first card if true stores src in first_card_clicked
+    if(first_card_clicked == null) {
+        //stores first_card_clicked var with src
+         first_card_clicked = $(element).prev().find("img");
+       }
+    else{
+        // stores src in second_card_clicked var
+        second_card_clicked = $(element).prev().find("img");
+        //check if we have a match
+             if(first_card_clicked.attr('src') == second_card_clicked.attr('src')){
+                 $(first_card_clicked).addClass('match');
+                 $(second_card_clicked).addClass('match');
+                 console.log('first set ', first_card_clicked, second_card_clicked);
+
+                 first_card_clicked = null;
+                 second_card_clicked = null;
+                 match_counter ++;
+                     if(match_counter == 9){
+                         $('#win').css('visibility', 'visible');
+                     }
+
+                 console.log(match_counter);
+                }
+            else{
+                 first_card_clicked = null;
+                 second_card_clicked = null;
+                console.log('second set ', first_card_clicked, second_card_clicked);
+             }
+        }
+}
 
 
-});
+
+
+
